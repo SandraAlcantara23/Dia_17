5b0647712d7f5c6c6efadaf9f1e71a7c4057779b
SandraAlcantara23 <160050434+SandraAlcantara23@users.noreply.github.com>
2024-03-17 19:51:57 -0600

    Add files via upload

---
+++ sandy.c
@@ -0,0 +1,12 @@
+int main() {
+    int num1, num2, suma;
+
+    printf("Ingresa dos números para sumar:\n");
+    scanf("%d %d", &num1, &num2);
+
+    suma = num1 + num2;
+
+    printf("La suma de %d y %d es: %d\n", num1, num2, suma);
+
+    return 0;
+}
